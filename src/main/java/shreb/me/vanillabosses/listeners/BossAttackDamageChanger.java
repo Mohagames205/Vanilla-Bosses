196ac51cbe9b7803da71538c10ccb80d5a730884
Shreb <Sebastian.Werberger@stud.th-rosenheim.de>
2022-02-14 15:21:48 +0100

    changed damage modifiers to utilize the Bosses enum

--- src/main/java/shreb/me/vanillabosses/listeners/BossAttackDamageChanger.java
+++ src/main/java/shreb/me/vanillabosses/listeners/BossAttackDamageChanger.java
@@ -5,6 +5,7 @@ import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import shreb.me.vanillabosses.bossclasses.Bosses;
 import shreb.me.vanillabosses.main.Main;
 
 public class BossAttackDamageChanger implements Listener {
@@ -19,61 +20,61 @@ public class BossAttackDamageChanger implements Listener {
 
             case ARROW:
                 if(e.getScoreboardTags().contains("applyModifier")){
-                    event.setDamage(event.getDamage() * config.getDouble("Bosses.SkeletonBoss.DamageModifier"));
+                    event.setDamage(event.getDamage() * Bosses.SKELETON.damageModifier);
                 }
 
             case SKELETON:
 
                 if(e.getScoreboardTags().contains("BossSkeleton")){
-                    event.setDamage(event.getDamage() * config.getDouble("Bosses.SkeletonBoss.DamageModifier"));
+                    event.setDamage(event.getDamage() * Bosses.SKELETON.damageModifier);
                 }
                 break;
 
             case CREEPER:
                 if(e.getScoreboardTags().contains("BossCreeper")){
-                    event.setDamage(event.getDamage() * config.getDouble("Bosses.CreeperBoss.DamageModifier"));
+                    event.setDamage(event.getDamage() * Bosses.CREEPER.damageModifier);
                 }
                 break;
 
             case SPIDER:
                 if(e.getScoreboardTags().contains("BossSpider")){
-                    event.setDamage(event.getDamage() * config.getDouble("Bosses.SpiderBoss.DamageModifier"));
+                    event.setDamage(event.getDamage() * Bosses.SPIDER.damageModifier);
                 }
                 break;
 
             case BLAZE:
                 if(e.getScoreboardTags().contains("BossBlaze")){
-                    event.setDamage(event.getDamage() * config.getDouble("Bosses.BlazeBoss.DamageModifier"));
+                    event.setDamage(event.getDamage() * Bosses.BLAZE.damageModifier);
                 }
                 break;
 
             case ENDERMAN:
                 if(e.getScoreboardTags().contains("BossEnderman")){
-                    event.setDamage(event.getDamage() * config.getDouble("Bosses.EndermanBoss.DamageModifier"));
+                    event.setDamage(event.getDamage() * Bosses.ENDERMAN.damageModifier);
                 }
                 break;
 
             case ZOMBIE:
                 if(e.getScoreboardTags().contains("BossZombie")){
-                    event.setDamage(event.getDamage() * config.getDouble("Bosses.ZombieBoss.DamageModifier"));
+                    event.setDamage(event.getDamage() * Bosses.ZOMBIE.damageModifier);
                 }
                 break;
 
             case ZOMBIFIED_PIGLIN:
                 if(e.getScoreboardTags().contains("BossZombified_Piglin")){
-                    event.setDamage(event.getDamage() * config.getDouble("Bosses.Zombified_PiglinBoss.DamageModifier"));
+                    event.setDamage(event.getDamage() * Bosses.ZOMBIEPIGMAN.damageModifier);
                 }
                 break;
 
             case SLIME:
                 if(e.getScoreboardTags().contains("BossSlime")){
-                    event.setDamage(event.getDamage() * config.getDouble("Bosses.SlimeBoss.DamageModifier"));
+                    event.setDamage(event.getDamage() * Bosses.SLIME.damageModifier);
                 }
                 break;
 
             case WITHER:
                 if(e.getScoreboardTags().contains("BossWither")){
-                    event.setDamage(event.getDamage() * config.getDouble("Bosses.WitherBoss.DamageModifier"));
+                    event.setDamage(event.getDamage() * Bosses.WITHER.damageModifier);
                 }
                 break;
         }

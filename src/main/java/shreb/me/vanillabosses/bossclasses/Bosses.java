cbc42d7d3e0dab5020c4cac74c17d83d7423262f
Shreb <Sebastian.Werberger@stud.th-rosenheim.de>
2022-02-22 21:23:01 +0100

    Added the ScoreboardTag String which identifies bosses in this plugin

--- src/main/java/shreb/me/vanillabosses/bossclasses/Bosses.java
+++ src/main/java/shreb/me/vanillabosses/bossclasses/Bosses.java
@@ -10,65 +10,83 @@ public enum Bosses {
     BLAZE(Main.getInstance().getConfig().getInt("Bosses.BlazeBoss.spawnChance"),
             Main.getInstance().getConfig().getDouble("Bosses.BlazeBoss.health"),
             Main.getInstance().getConfig().getDouble("Bosses.BlazeBoss.DamageModifier"),
-            "BlazeBoss"),
+            "BlazeBoss",
+            "BossBlaze"),
 
     CREEPER(Main.getInstance().getConfig().getInt("Bosses.CreeperBoss.spawnChance"),
             Main.getInstance().getConfig().getDouble("Bosses.CreeperBoss.health"),
             Main.getInstance().getConfig().getDouble("Bosses.CreeperBoss.DamageModifier"),
-            "CreeperBoss"),
+            "CreeperBoss",
+            "BossCreeper"),
 
     ENDERMAN(Main.getInstance().getConfig().getInt("Bosses.EndermanBoss.spawnChance"),
             Main.getInstance().getConfig().getDouble("Bosses.EndermanBoss.health"),
             Main.getInstance().getConfig().getDouble("Bosses.EndermanBoss.DamageModifier"),
-            "EndermanBoss"),
+            "EndermanBoss",
+            "BossEnderman"),
 
     SKELETON(Main.getInstance().getConfig().getInt("Bosses.SkeletonBoss.spawnChance"),
             Main.getInstance().getConfig().getDouble("Bosses.SkeletonBoss.health"),
             Main.getInstance().getConfig().getDouble("Bosses.SkeletonBoss.DamageModifier"),
-            "SkeletonBoss"),
+            "SkeletonBoss",
+            "BossSkeleton"),
 
     SLIME(Main.getInstance().getConfig().getInt("Bosses.SlimeBoss.spawnChance"),
             Main.getInstance().getConfig().getDouble("Bosses.SlimeBoss.health"),
             Main.getInstance().getConfig().getDouble("Bosses.SlimeBoss.DamageModifier"),
-            "SlimeBoss"),
+            "SlimeBoss",
+            "BossSlime"),
 
     SPIDER(Main.getInstance().getConfig().getInt("Bosses.SpiderBoss.spawnChance"),
             Main.getInstance().getConfig().getDouble("Bosses.SpiderBoss.health"),
             Main.getInstance().getConfig().getDouble("Bosses.SpiderBoss.DamageModifier"),
-            "SpiderBoss"),
+            "SpiderBoss",
+            "BossSpider"),
 
     WITCH(Main.getInstance().getConfig().getInt("Bosses.WitchBoss.spawnChance"),
             Main.getInstance().getConfig().getDouble("Bosses.WitchBoss.health"),
             1,                                             //damageModifier of 1, since there is no benefit in changing it for a witch.
-            "WitchBoss"),
+            "WitchBoss",
+            "BossWitch"),
 
     WITHER(0,                                               //Witherboss spawnchance of 0 since it doesnt spawn naturally
             Main.getInstance().getConfig().getDouble("Bosses.WitherBoss.Health"),
             Main.getInstance().getConfig().getDouble("Bosses.WitherBoss.DamageModifier"),
-            "WitherBoss"),
+            "WitherBoss",
+            "BossWither"),
 
     ZOMBIE(Main.getInstance().getConfig().getInt("Bosses.ZombieBoss.spawnChance"),
             Main.getInstance().getConfig().getDouble("Bosses.ZombieBoss.health"),
             Main.getInstance().getConfig().getDouble("Bosses.ZombieBoss.DamageModifier"),
-            "ZombieBoss"),
+            "ZombieBoss",
+            "BossZombie"),
 
     ZOMBIFIED_PIGLIN(Main.getInstance().getConfig().getInt("Bosses.Zombified_PiglinBoss.spawnChance"),
             Main.getInstance().getConfig().getDouble("Bosses.Zombified_PiglinBoss.health"),
             Main.getInstance().getConfig().getDouble("Bosses.Zombified_PiglinBoss.DamageModifier"),
-            "Zombified_PiglinBoss");
+            "Zombified_PiglinBoss",
+            "BossZombified_Piglin"),
+
+    MAGMACUBE(Main.getInstance().getConfig().getInt("Bosses.MagmacubeBoss.spawnChance"),
+            Main.getInstance().getConfig().getDouble("Bosses.MagmacubeBoss.health"),
+            Main.getInstance().getConfig().getDouble("Bosses.MagmacubeBoss.DamageModifier"),
+            "MagmacubeBoss",
+            "BossMagmacube");
 
 
     public int spawnChance; //out of 1000
     public double health;
     public double damageModifier;
     public String configSectionName;
+    public String scoreboardBossTag;
 
 
 
-    Bosses(int spawnChance, double health, double damageModifier, String configSectionName){
+    Bosses(int spawnChance, double health, double damageModifier, String configSectionName, String scoreboardBossTag){
         this.spawnChance = spawnChance;
         this.health = health;
         this.damageModifier = damageModifier;
         this.configSectionName = configSectionName;
+        this.scoreboardBossTag = scoreboardBossTag;
     }
 }
